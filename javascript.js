e12018eca82a053b2913e62e6e1b03e2fd5694f9
KukutlaSreekar <99190049+KukutlaSreekar@users.noreply.github.com>
2025-02-23 16:13:36 +0530

    Create function for computers choice

---
+++ javascript.js
@@ -0,0 +1,18 @@
+function getComputerChoice(){
+    let randNum=Math.floor(Math.random() * 3);
+    let compChoice;
+    if (randNum==0)
+    {
+        compChoice="ROCK";
+    }
+    else if (randNum==1)
+    {
+        compChoice="PAPER";
+    }
+    else
+    {
+        compChoice="SCISSORS";
+    }
+    return compChoice;
+    
+}
